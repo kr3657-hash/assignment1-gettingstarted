6d3e1c08fe5dcd8cdb096c2e2b320e85c80fb6ed
kr3657-hash <kr3657@nyu.edu>
2026-01-31 12:07:20 -0500

    gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -13,7 +13,6 @@ def welcome_assignment_answers(question):
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
         ### maybe put something here to flag an issue and catch errors
-        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
     return(answer)
 # Complete all the questions.
 
@@ -26,13 +25,14 @@ if __name__ == "__main__":
 #Questions:
 #"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":pcap
 #"Are encoding and encryption the same? - Yes/No": No
-#"Is it possible to decrypt a message without a key? - Yes/No": Yes
+#"Is it possible to decrypt a message without a key? - Yes/No": No
 #"Is it possible to decode a message without a key? - Yes/No": No
-#"Is a hashed message supposed to be un-hashed? - Yes/No": Yes
+#"Is a hashed message supposed to be un-hashed? - Yes/No": No
 #"What is the SHA256 hashing value of your NYU email and use the answer in your code - ": 8797c05a6c6936211ff56fc4869b6f7dc7b09a494cc59039783ccdd932288bfb
-#"Is MD5 a secured hashing algorithm? - Yes/No": Yes
+#"Is MD5 a secured hashing algorithm? - Yes/No": No
 #"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number": 4
 #"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number": 2
+
 
 
 
