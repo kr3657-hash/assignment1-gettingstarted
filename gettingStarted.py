544134d0eddb78c2a98390cda4644489a24f8318
kr3657-hash <kr3657@nyu.edu>
2026-01-31 11:55:39 -0500

    gettingStarted.py
    updated

--- gettingStarted.py
+++ gettingStarted.py
@@ -6,9 +6,9 @@ def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "No"
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
@@ -24,12 +24,13 @@ if __name__ == "__main__":
     print(welcome_assignment_answers(debug_question))
 
 #Questions:
-#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-#"Are encoding and encryption the same? - Yes/No":
-#"Is it possible to decrypt a message without a key? - Yes/No":
-#"Is it possible to decode a message without a key? - Yes/No":
-#"Is a hashed message supposed to be un-hashed? - Yes/No":
-#"What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-#"Is MD5 a secured hashing algorithm? - Yes/No":
-#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-#"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?": pcap
+#"Are encoding and encryption the same? - Yes/No": No
+#"Is it possible to decrypt a message without a key? - Yes/No": No
+#"Is it possible to decode a message without a key? - Yes/No": Yes
+#"Is a hashed message supposed to be un-hashed? - Yes/No": No
+#"What is the SHA256 hashing value of your NYU email and use the answer in your code - ": 8797c05a6c6936211ff56fc4869b6f7dc7b09a494cc59039783ccdd932288bfb
+#"Is MD5 a secured hashing algorithm? - Yes/No": No
+#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number": 4
+#"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number": 2
+
